fa6142b3c3eae2a0c165d90fc8dc8d602adb3a92
Bruno Neville <brunoneville31@gmail.com>
2020-03-16 12:40:18 -0300

    rota de sincronização

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   
   get '/woocommerce', to: 'produtos#woocommerce_list'
   get '/ideal', to:'produtos#ideal_soft_list'
+  post '/sincronizar', to: 'sincronizacao#home'
 
   root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
